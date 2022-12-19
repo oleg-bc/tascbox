d1e3002df852205945c839bcba862d808a6ce303
Oleg.hansen <oleg.hansen@anthem.com>
2022-12-19 14:09:07 -0800

    tut commit

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,22 @@
+
+import '../src/index.css';
+
+// Registers the msw addon
+import { initialize, mswDecorator } from 'msw-storybook-addon';
+
+// Initialize MSW
+initialize();
+
+// Provide the MSW addon decorator globally
+export const decorators = [mswDecorator];
+
+//👇 Configures Storybook to log the actions( onArchiveTask and onPinTask ) in the UI.
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
+ actions: { argTypesRegex: '^on[A-Z].*' },
+ controls: {
+   matchers: {
+     color: /(background|color)$/i,
+     date: /Date$/,
+   },
+ },
 };
